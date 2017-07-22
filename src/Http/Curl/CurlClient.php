a8fa38d3628c565eb918fe8ac943e9a8d29723fa
Tom Myers <tmyers273@gmail.com>
2017-07-22 10:35:59 -0400

    Roughs out curl client

--- src/Http/Curl/CurlClient.php
+++ src/Http/Curl/CurlClient.php
@@ -6,9 +6,17 @@ use tmyers273\IceScraper\ScraperResponse;
 
 class CurlClient {
 
-    public function get() : ScraperResponse
+    public function get($url) : ScraperResponse
     {
+        $ch = curl_init($url);
 
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $result = curl_exec($ch);
+        curl_close($ch);
+
+        $response = new ScraperResponse($result);
+        return $response;
     }
 
 }
